02ccf8c0db3892bf924ae204f34aa4c16bd72d3a
Nujhath <nujhath9@gmail.com>
2026-07-20 00:51:13 +0600

    scholarship add form created

---
+++ add.js
@@ -0,0 +1,32 @@
+const formInfo = document.getElementById("formInfo");
+
+formInfo.addEventListener("submit", function (event) {
+   
+    const title = document.getElementById("title").value;
+    const location = document.getElementById("location").value;
+    const deadline = document.getElementById("deadline").value;
+    const link = document.getElementById("link").value;
+    const type = document.getElementById("type").value;
+    const hour = document.getElementById("hour").value;
+    const language = document.getElementById("language").value;
+    const description = document.getElementById("description").value;
+
+    const scholarship = {
+        title,
+        location,
+        deadline,
+        link,
+        type,
+        hour,
+        language,
+        description
+    };
+
+       const lists = JSON.parse(localStorage.getItem("list")) || [];
+
+        lists.push(scholarship);
+    localStorage.setItem("list", JSON.stringify(lists));
+
+    console.log("Scholarship saved!");
+
+});
